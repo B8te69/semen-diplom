82881ac2a7e21af60600a2a6a3c1732c4c5ccea9
B8te69Э <b8te88@gmail.com>
2021-05-27 22:54:19 +0300

    feat

--- index.ts
+++
@@ -1,1 +0,0 @@
-console.log('spat hoc1)
---
+++ js/scrollTop.js
@@ -0,0 +1,29 @@
+let t;
+function up() {
+  let top = Math.max(
+    document.body.scrollTop,
+    document.documentElement.scrollTop
+  );
+  if (top > 0) {
+    window.scrollBy(0, -500);
+    t = setTimeout("up()", 40);
+  } else clearTimeout(t);
+
+  return false;
+}
+
+window.addEventListener("scroll", () => {
+  let btn = document.querySelector(".btn-top");
+  let topOff = 800;
+  let OffTop = window.pageYOffset
+    ? window.pageYOffset
+    : document.body.scrollTop;
+
+  if (OffTop >= 450 && OffTop <= 1000) {
+    btn.classList.remove("hide");
+    btn.classList.add("show");
+  } else {
+    btn.classList.remove("show");
+    btn.classList.add("hide");
+  }
+});
